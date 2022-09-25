a75b26e61a97402b08d01413eaeb744d3b81a326
justicepelteir <justice.douglas@hey.com>
2022-09-25 15:49:56 -0700

    add postgres placeholders

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "database_name",
+        "USER": "user_name",
+        "PASSWORD": "your_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
