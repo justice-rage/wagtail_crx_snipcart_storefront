bf75040e390f91ff6e8cd0afee42a4ef72444c33
justicepelteir <justice.douglas@hey.com>
2022-09-26 20:55:46 -0700

    add initial product models

--- website/models.py
+++ website/models.py
@@ -11,6 +11,12 @@ from coderedcms.models import (
     CoderedWebPage,
 )
 
+# Developer Added
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.images import get_image_model_string
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class ArticlePage(CoderedArticlePage):
     """
@@ -84,3 +90,63 @@ class WebPage(CoderedWebPage):
         verbose_name = "Web Page"
 
     template = "coderedcms/pages/web_page.html"
+
+# ------ E-Commerce Section: Snipcart ------
+
+class ProductsIndexPage(CoderedWebPage):
+    # Landing page for Products
+    class Meta:
+        verbose_name = "Products Landing Page"
+
+    # Override to specify custom index ordering choice/default.
+    index_query_pagemodel = 'website.ProductsPage'
+
+    # Only allow ProductPages beneath this page.
+    subpage_types = ['website.ProductsPage']
+
+    template = 'website/pages/products_index_page.html'
+
+class ProductsPage(CoderedWebPage):
+    # Custom page for individual products
+
+    class Meta:
+        verbose_name = "Products Page"
+
+    # Only allow this page to be created beneath a ProductsIndexPage.
+    parent_page_types = ['website.ProductsIndexPage']
+
+    template = "website/pages/products_page.html"
+
+    # Products Page model fields
+    description = RichTextField(
+        verbose_name="Products Description",
+        null=True,
+        blank=True,
+        default=""
+    )
+    photo = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name='Product Photo',
+    )
+    DAYS_CHOICES = (
+        ("Weekends Only", "Weekends Only"),
+        ("Monday-Friday", "Monday-Friday"),
+        ("Tuesday/Thursday", "Tuesday/Thursday"),
+        ("Seasonal", "Seasonal"),
+    )
+    days_available = models.CharField(
+        choices = DAYS_CHOICES,
+        max_length=20,
+        default=""
+    )
+
+    # Add custom fields to the body
+    body_content_panels = CoderedWebPage.body_content_panels + [
+        FieldPanel("description"),
+        FieldPanel("photo"),
+        FieldPanel("days_available"),
+    ]
